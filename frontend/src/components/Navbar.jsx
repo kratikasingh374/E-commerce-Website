f6e107f828a421f4759c06e836accc3ad5ec8c96
Kratika Singh <184718043+kratikasingh374@users.noreply.github.com>
2026-04-03 10:28:32 +0530

    Initial commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,75 @@
+import styles from "./Navbar.module.css";
+import { Search, Menu, X, User, Heart, ShoppingCart } from "lucide-react";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav id={styles.navbar}>
+      
+      {/* LOGO */}
+      <div id={styles.logo}>
+        <div id={styles.logoImage}>
+          <img
+            src="https://previews.123rf.com/images/dahlangraphic/dahlangraphic1911/dahlangraphic191100303/133838633-infinity-logo-design-inspiration-vector-illustration-template.jpg"
+            alt=""
+            id={styles.imageofLogo}
+          />
+        </div>
+        <p className={styles.logoName}>Infinity</p>
+      </div>
+
+      <div id={styles.main}>
+
+        {/* SEARCH */}
+        <div id={styles.searchSection}>
+          <input
+            type="text"
+            placeholder="Search products..."
+            id={styles.searchBar}
+          />
+          <button id={styles.searchButton}>
+            <Search size={18} />
+          </button>
+        </div>
+
+
+        {/* MENU ICON */}
+        <div
+          className={styles.menuIcon}
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <X size={24} /> : <Menu size={24} />}
+        </div>
+
+        {/* LINKS */}
+        <ul className={`${styles.links} ${open ? styles.showMenu : ""}`}>
+          <li><a href="/">Home</a></li>
+          <li><a href="/products">Products</a></li>
+          <li><a href="/categories">Categories</a></li>
+        </ul>
+
+        {/* ICON SECTION (moved outside ul) */}
+        <div className={styles.iconSection}>
+          <div className={styles.iconBox}>
+            <User size={22} />
+            <span>Profile</span>
+          </div>
+
+          <div className={styles.iconBox}>
+            <Heart size={22} />
+            <span>Wishlist</span>
+          </div>
+
+          <div className={styles.iconBox}>
+            <ShoppingCart size={22} />
+            <span>Cart</span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
